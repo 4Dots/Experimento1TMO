2b90ff5e389a49f29d2feabedaded76692d15bc3
estudiante <estudiante@ISIS2503-35.virtual.uniandes.edu.co>
2014-09-17 21:58:46 -0500

    Added Libraries - Basic Classes

---
+++ Experimento1-TMO-war/src/java/fourDots/TMO/beans/LoginManagedBean.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fourDots.TMO.beans;
+
+/**
+ *
+ * @author estudiante
+ */
+public class LoginManagedBean
+{
+
+    /**
+     * Creates a new instance of LoginManagedBean
+     */
+    public LoginManagedBean()
+    {
+    }
+    
+}
